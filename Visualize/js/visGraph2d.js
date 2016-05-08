c317d6409a2c653876b86ebad6982a86c7973dcc
Brett Barwick <bbarwick@uscupstate.edu>
2016-05-07 20:50:17 -0400

    Added missing semicolons
    Added some missing semicolons

--- Visualize/js/visGraph2d.js
+++ Visualize/js/visGraph2d.js
@@ -852,11 +852,11 @@ function exportTikz (event){
 function highlightAllNeighbors(n) {
     circle.selectAll('circle')
     // Highlight all nodes that are neighbors with the given node n.
-    .classed("highlighted", function(d) { return (areNeighbors(d,n)) })
+    .classed("highlighted", function(d) { return areNeighbors(d,n); });
 }
 
 function areNeighbors(node1,node2) {
-    return links.some( function(l) {(l.source === node1 && l.target === node2) || (l.target === node1 && l.source === node 2)} );    
+    return links.some( function(l) {return ((l.source === node1 && l.target === node2) || (l.target === node1 && l.source === node 2));} );    
 }
 
 // -----------------------------------------
